0be5412917bcddf42e8e46013a5092da3ac4bcb8
dev-kibeom <neopkrrl@gmail.com>
2026-05-11 19:36:47 +0900

    merge done

--- src/cobot2/cobot_core/cobot_core/actions/logical_actions/pour.py
+++ src/cobot2/cobot_core/cobot_core/actions/logical_actions/pour.py
@@ -1,23 +1,23 @@
 from ..base_action import BaseAction
+import time
 
 class Pour(BaseAction):
-    """용기를 기울여 내용물을 붓고 제자리로 돌아오는 동작"""
+    """용기를 자연스럽게 기울여 내용물을 붓고 제자리로 돌아오는 동작"""
     action_name = 'pour'
 
-    def execute(self, pos, angle=100, wait_time=2.0):
-        # 1. 냄비 위 안전 고도로 이동
-        if not self.manager.perform('approach', pos=pos, offset=150): return False
-        
-        # 2. 손목(Ry 또는 Rx)을 angle만큼 꺾어서 붓기 (Tool 기준 상대 회전)
-        if not self.manager.perform('movel', pos=[0, 0, 0, 0, angle, 0], mode='rel', ref='tool'): return False
-        
-        # 3. 내용물이 다 떨어질 때까지 대기
-        self.wait(wait_time)
-        
-        # 4. 손목 원상 복구 (반대 방향 회전)
-        if not self.manager.perform('movel', pos=[0, 0, 0, 0, -angle, 0], mode='rel', ref='tool'): return False
-        
-        # 5. 위로 빠져나오기
-        if not self.manager.perform('movel', pos=[0, 0, -150, 0, 0, 0], mode='rel', ref='tool'): return False
-        
-        return True
+    def execute(self):  
+        current_joint = self.get_current_posj()
+        if 85 <= current_joint[2] <= 95 and 85 <= current_joint[4] <= 95 :
+            vertical_current_pos = self.get_current_posx()
+            if not self.manager.perform('movej', joint=[0,0,0,0,0,90], vel=50, acc=50, mode='rel'): return False      
+            if not self.manager.perform('movel', pos=[312,-198,187,vertical_current_pos[3],vertical_current_pos[4],vertical_current_pos[5]],vel=100,acc=100,mode='abs'): return False
+            if not self.manager.perform('movel', pos=[0,0,0,0,35,90], vel=50, acc=50, mode='rel', ref='tool'): return False
+            self.wait(time=3)
+            if not self.manager.perform('movej', joint=[0,0,90,0,90,0], vel=100, acc=100, mode='abs'): return False
+        # 붓는 위치로 이동
+        else:
+            horizontal_current_pos = self.get_current_posx()
+            if not self.manager.perform('movel', pos=[312,-198,187,horizontal_current_pos[3],horizontal_current_pos[4],horizontal_current_pos[5]],vel=100,acc=100): return False
+            if not self.manager.perform('movej',joint=[0,0,0,0,0,90],vel=100,acc=100,mode='rel'): return False
+            self.wait(time=3)
+            if not self.manager.perform('movej',joint=[0,0,0,0,0,-90],vel=100,acc=100,mode='rel'): return False
