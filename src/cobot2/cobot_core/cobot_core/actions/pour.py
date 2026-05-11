ae98281904f74c27c757a14fd1636d2def54bce7
KJH <kjhung8@gmail.com>
2026-05-11 11:23:05 +0900

    new file:   detection_results/detect_20260511_111251_522_shaker_color.jpg 	new file:   detection_results/detect_20260511_111251_522_shaker_depth.jpg 	new file:   detection_results/detect_20260511_111300_712_shaker_color.jpg 	new file:   detection_results/detect_20260511_111300_712_shaker_depth.jpg 	new file:   detection_results/detect_20260511_111310_669_shaker_color.jpg 	new file:   detection_results/detect_20260511_111310_669_shaker_depth.jpg 	new file:   detection_results/detect_20260511_111316_136_shaker_color.jpg 	new file:   detection_results/detect_20260511_111316_136_shaker_depth.jpg 	new file:   detection_results/detect_20260511_111326_394_shaker_color.jpg 	new file:   detection_results/detect_20260511_111326_394_shaker_depth.jpg 	modified:   src/cobot2/cobot_core/cobot_core/actions/pour.py 	modified:   src/cobot2/voice_processing/resource/commands.txt 	modified:   src/cobot2/voice_processing/resource/sequence.json 	modified:   src/cobot2/voice_processing/voice_processing/prompt.py

--- src/cobot2/cobot_core/cobot_core/actions/pour.py
+++ src/cobot2/cobot_core/cobot_core/actions/pour.py
@@ -5,26 +5,26 @@ class Pour(BaseAction):
     """용기를 자연스럽게 기울여 내용물을 붓고 제자리로 돌아오는 동작"""
     action_name = 'pour'
 
-    def execute(self):
-        # pos = self.manager.get_vision_target(target)
+    def execute(self, target=None):
+        pos = self.manager.get_vision_target(target)
 
-        # if not pos:
-        #     if not self.manager.perform('finding', target=target):
-        #         print(f"❌ '{target}' finding 실패")
-        #         return False
+        if not pos:
+            if not self.manager.perform('finding', target=target):
+                print(f"❌ '{target}' finding 실패")
+                return False
 
-        #     pos = self.manager.target_pos
+            pos = self.manager.get_vision_target()
 
-        #     if not pos:
-        #         print(f"❌ '{target}' finding 후에도 좌표가 없습니다.")
-        #         return False
+            if not pos:
+                print(f"❌ '{target}' finding 후에도 좌표가 없습니다.")
+                return False
                 
         # 붓는 위치로 이동
         current_pos = self.get_current_posx()
         if not self.manager.perform('movel', pos=[pos[0],pos[1],pos[2]+150,current_pos[3],current_pos[4],current_pos[5]],vel=100,acc=100): return False
         
 
-        if not self.manager.perform('movej',joint=[0,0,0,0,0,30],vel=100,acc=100,mode='rel',ref='base'): return False
+        if not self.manager.perform('movej',joint=[0,0,0,0,0,45],vel=100,acc=100,mode='rel',ref='base'): return False
 
         # 부을 때 까지 대기
         if not self.manager.perform('wait', time=3): return False
